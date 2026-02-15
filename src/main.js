86b85d8d12f1fb4197df72f9d99dd3b52baff0bc
Camilla Carvalho <camillacarvalho037@gmail.com>
2026-02-15 12:04:00 -0300

    refactor: altera nome de arquivo de rota

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import router from './Rotas/routes.js'
+import router from './Routes/routes.js'
 
 createApp(App).use(router).mount('#app')
